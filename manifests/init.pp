01eb59419eb8f602217652fc5bb9a4f9d8f157c1
Christer Ekholm <che@chrekh.se>
2015-07-12 23:00:21 +0200

    Added parameters for determine if only one addres should be used.
    The default is true because that is what the previous version which
    used core-facts did.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,23 +3,35 @@
 # Manage /etc/hosts
 #
 class hosts (
-  $file          = '/etc/hosts',
-  $lo_ipv4       = $ipv4_lo_addrs,
-  $lo_ipv6       = $ipv6_lo_addrs,
-  $lo_names      = [ 'localhost' ],
-  $primary_ipv4  = $ipv4_pri_addrs,
-  $primary_ipv6  = $ipv6_pri_addrs,
-  $primary_names = [ $::fqdn, $::hostname ],
-  $entries       = {},
+  $file             = '/etc/hosts',
+  $lo_ipv4          = $ipv4_lo_addrs,
+  $lo_ipv6          = $ipv6_lo_addrs,
+  $lo_names         = [ 'localhost' ],
+  $one_primary_ipv4 = true,
+  $one_primary_ipv6 = true,
+  $primary_ipv4     = $ipv4_pri_addrs,
+  $primary_ipv6     = $ipv6_pri_addrs,
+  $primary_names    = [ $::fqdn, $::hostname ],
+  $entries          = {},
 ) {
   validate_string($file)
   validate_array($lo_ipv4)
   validate_array($lo_ipv6)
   validate_array($lo_names)
+  validate_bool($one_primary_ipv4)
+  validate_bool($one_primary_ipv6)
   validate_array($primary_ipv4)
   validate_array($primary_ipv6)
   validate_array($primary_names)
   validate_hash($entries)
+  $pri_ipv4 = $one_primary_ipv4 ? {
+    true    => [ $primary_ipv4[0] ],
+    default => $primary_ipv4,
+  }
+  $pri_ipv6 = $one_primary_ipv6 ? {
+    true    => [ $primary_ipv6[0] ],
+    default => $primary_ipv6,
+  }
   file { $file:
     ensure  => present,
     owner   => 'root',
