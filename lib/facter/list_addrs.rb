2a9a9ce119b4af474c529101fa23d617c57e42c9
Christer Ekholm <che@chrekh.se>
2015-07-13 00:13:09 +0200

    Changed from getifaddrs to ip_address_list
    To support older ruby than 2.1

--- lib/facter/list_addrs.rb
+++ lib/facter/list_addrs.rb
@@ -5,17 +5,15 @@ primary_ipv4 = Array.new
 lo_ipv6 = Array.new
 primary_ipv6 = Array.new
 
-Socket.getifaddrs.each.each { |ifaddr|
-  if ifaddr.flags & Socket::IFF_LOOPBACK != 0
-    if ifaddr.addr.ipv4_loopback? && ifaddr.addr.ip_address == '127.0.0.1'
-      lo_ipv4 << ifaddr.addr.ip_address
-    elsif ifaddr.addr.ipv6_loopback?
-      lo_ipv6 << ifaddr.addr.ip_address
-    end
-  elsif ifaddr.addr.ipv4? && !ifaddr.addr.ipv4_multicast?
-    primary_ipv4 << ifaddr.addr.ip_address
-  elsif ifaddr.addr.ipv6? && !ifaddr.addr.ipv6_linklocal? && !ifaddr.addr.ipv6_multicast?
-    primary_ipv6 << ifaddr.addr.ip_address
+Socket.ip_address_list.each { |addr|
+  if addr.ipv4_loopback?
+    lo_ipv4 << addr.ip_address
+  elsif addr.ipv6_loopback?
+    lo_ipv6 << addr.ip_address
+  elsif addr.ipv4? && !addr.ipv4_multicast?
+    primary_ipv4 << addr.ip_address
+  elsif addr.ipv6? && !addr.ipv6_linklocal? && !addr.ipv6_multicast?
+    primary_ipv6 << addr.ip_address
   end
 }
 
