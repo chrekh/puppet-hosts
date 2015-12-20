3caba31d6a482451cfb6fbaf1d6ca557a418b978
Frank Wall <fw@moov.de>
2015-12-20 21:40:29 +0100

    fix undefined variables

--- lib/facter/list_addrs.rb
+++ lib/facter/list_addrs.rb
@@ -11,19 +11,20 @@ else
   require 'facter/util/ip'
   addrs = Array.new
   Facter::Util::IP.get_interfaces.each do |interface|
-    addrs << Facter::Util::IP.get_interface_value(interface,ipaddress)
-    addrs << Facter::Util::IP.get_interface_value(interface,ipaddress6)
+    %w{ipaddress ipaddress6}.each do |label|
+      addrs << Facter::Util::IP.get_interface_value(interface,label)
+    end
   end
 end
 
 addrs.each { |addr|
-  if addr.ipv4_loopback?
+  if defined? addr.ipv4_loopback && addr.ipv4_loopback?
     lo_ipv4 << addr.ip_address
-  elsif addr.ipv6_loopback?
+  elsif defined? addr.ipv6_loopback && addr.ipv6_loopback?
     lo_ipv6 << addr.ip_address
-  elsif addr.ipv4? && !addr.ipv4_multicast?
+  elsif defined? addr.ipv4 && addr.ipv4? && !addr.ipv4_multicast?
     primary_ipv4 << addr.ip_address
-  elsif addr.ipv6? && !addr.ipv6_linklocal? && !addr.ipv6_multicast?
+  elsif defined? addr.ipv6 && addr.ipv6? && !addr.ipv6_linklocal? && !addr.ipv6_multicast?
     primary_ipv6 << addr.ip_address
   end
 }
