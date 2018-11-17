4ce64a6728a95afb5a476795f1bc83ea4edf0bdb
Christer Ekholm <che@chrekh.se>
2018-11-17 20:26:07 +0100

    Add local facts to init_spec.rb
    It would obviously be better so actually run the fact list_addrs.rb to
    get that tested also.  But I don't know how to do that.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 describe 'hosts' do
+  let(:facts) do
+    {
+      'ipv4_lo_addrs'  => '127.0.0.1',
+      'ipv6_lo_addrs'  => '::1',
+      'ipv4_pri_addrs' => '192.168.2.1',
+      'ipv6_pri_addrs' => '2001:db8:abba::1',
+    }
+  end
+
   context 'with defaults for all parameters' do
     it { is_expected.to contain_class('hosts') }
   end
