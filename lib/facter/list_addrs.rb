4ef0bf504c93d6dbbd50ef4348c87e43b0d27af8
Christer Ekholm <che@chrekh.se>
2018-11-17 18:24:19 +0100

    Modifications by pdk validate --audo-corret

--- lib/facter/list_addrs.rb
+++ lib/facter/list_addrs.rb
@@ -1,10 +1,10 @@
 require 'socket'
 
-lo_ipv4 = [ Facter.value('ipaddress_lo') ].compact
-primary_ipv4 = [ Facter.value('ipaddress') ].compact
-lo_ipv6 = [ Facter.value('ipaddress6_lo') ].compact
-primary_ipv6 = [ Facter.value('ipaddress6') ].compact
-if primary_ipv6[0] =~ /^fe80::/i
+lo_ipv4 = [Facter.value('ipaddress_lo')].compact
+primary_ipv4 = [Facter.value('ipaddress')].compact
+lo_ipv6 = [Facter.value('ipaddress6_lo')].compact
+primary_ipv6 = [Facter.value('ipaddress6')].compact
+if primary_ipv6[0] =~ %r{^fe80::}i
   primary_ipv6 = []
 end
 
@@ -15,20 +15,20 @@ if defined? Socket.ip_address_list
   rescue
   end
 end
-if ! addrs and defined? Socket.getifaddrs
+if !addrs && defined? Socket.getifaddrs
   begin
     addrs = Socket.getifaddrs.map { |i| i.addr }
   rescue ScriptError
   rescue
   end
 end
-if ! addrs
-  addrs = Array.new
+unless addrs
+  addrs = []
   begin
     require 'facter/util/ip'
     Facter::Util::IP.get_interfaces.each do |interface|
-      %w{ipaddress ipaddress6}.each do |label|
-        addrs << Facter::Util::IP.get_interface_value(interface,label)
+      ['ipaddress', 'ipaddress6'].each do |label|
+        addrs << Facter::Util::IP.get_interface_value(interface, label)
       end
     end
   rescue ScriptError
@@ -36,7 +36,7 @@ if ! addrs
   end
 end
 
-addrs.each { |addr|
+addrs.each do |addr|
   if (defined? addr.ipv4_loopback?) && addr.ipv4_loopback?
     lo_ipv4 << addr.ip_address
   elsif (defined? addr.ipv6_loopback?) && addr.ipv6_loopback?
@@ -46,7 +46,7 @@ addrs.each { |addr|
   elsif (defined? addr.ipv6?) && addr.ipv6? && !addr.ipv6_linklocal? && !addr.ipv6_multicast?
     primary_ipv6 << addr.ip_address
   end
-}
+end
 
 Facter.add('ipv4_lo_addrs') do
   setcode do
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 describe 'hosts' do
-
   context 'with defaults for all parameters' do
-    it { should contain_class('hosts') }
+    it { is_expected.to contain_class('hosts') }
   end
 end
