ac39598820cad4e89d4309636355a3f2e67a1a93
Christer Ekholm <che@chrekh.se>
2021-03-27 17:59:57 +0100

    Fix PDK warning "Useless assignment to variable - `result`."

--- lib/facter/list_addrs.rb
+++ lib/facter/list_addrs.rb
@@ -50,21 +50,21 @@ end
 
 Facter.add('ipv4_lo_addrs') do
   setcode do
-    result = lo_ipv4.uniq.sort.join(' ')
+    lo_ipv4.uniq.sort.join(' ')
   end
 end
 Facter.add('ipv4_pri_addrs') do
   setcode do
-    result = primary_ipv4.uniq.sort.join(' ')
+    primary_ipv4.uniq.sort.join(' ')
   end
 end
 Facter.add('ipv6_lo_addrs') do
   setcode do
-    result = lo_ipv6.uniq.sort.join(' ')
+    lo_ipv6.uniq.sort.join(' ')
   end
 end
 Facter.add('ipv6_pri_addrs') do
   setcode do
-    result = primary_ipv6.uniq.sort.join(' ')
+    primary_ipv6.uniq.sort.join(' ')
   end
 end
