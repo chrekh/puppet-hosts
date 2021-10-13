99097033da0ce9e4a4bda076586cb30e03e10305
Christer Ekholm <che@chrekh.se>
2021-10-13 19:52:11 +0200

    Fix invalid IPv6 addrs in testdata

--- spec/functions/collect_other_spec.rb
+++ spec/functions/collect_other_spec.rb
@@ -16,7 +16,7 @@ describe 'hosts::collect_other' do
   it {
     is_expected.to run.with_params('ip6').and_return(['2001:db8:abba::1',
                                                       '2001:db8:abba::2',
-                                                      '2001:db8::42::1',
+                                                      '2001:db8::42:1',
                                                       '2001:db8:dd:42::1'])
   }
 end
