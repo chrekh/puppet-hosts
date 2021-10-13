53adbb98341da0ea811f3c89b650ddd064e178b8
Christer Ekholm <che@chrekh.se>
2021-10-13 18:08:17 +0200

    Remove unused simulated test-facts.
    And move the still used fact osfamily to default_module_facts.yml

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,15 +1,5 @@
 require 'spec_helper'
 describe 'hosts' do
-  let(:facts) do
-    {
-      'ipv4_lo_addrs'  => '127.0.0.1',
-      'ipv6_lo_addrs'  => '::1',
-      'ipv4_pri_addrs' => '192.168.2.1',
-      'ipv6_pri_addrs' => '2001:db8:abba::1',
-      'osfamily'       => 'Gentoo',
-    }
-  end
-
   context 'with defaults for all parameters' do
     it { is_expected.to contain_class('hosts') }
   end
